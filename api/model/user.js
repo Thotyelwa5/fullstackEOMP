a734276b90e3b8913c082452a88b57e20dac99c6
Thotyelwa <mpongwanathotyelwa@gmail.com>
2023-08-21 13:50:35 +0200

    update

--- api/model/user.js
+++ api/model/user.js
@@ -1,139 +1,195 @@
-const db = require("../config") //this imprt the db con from config
-const {hash, compare, hashSync} = require('bcrypt')
-const {createToken} = require('../middleware/authentication')
+const db = require("../config"); //this imprt the db con from config
+const { hash, compare, hashSync } = require("bcrypt");
+const { createToken } = require("../middleware/authentication");
 
-class Users{
-    fetchUsers(req, res){
-        const query = `
+class Users {
+  fetchUsers(req, res) {
+    const query = `
         SELECT userID, firstName, lastNmae,userAge, gender,userRole,
         emailAdd, profileUrl
         FROM Users;
-        `
-        db.query(query,(err, results)=>{
-            if(err)throw err
-            res.json({
-                status: res.stausCode,
-                results
-            })
-        })
-    }
-    fetchUser(req, res){
-        const query = `
+        `;
+    db.query(query, (err, results) => {
+      if (err) throw err;
+      res.json({
+        status: res.stausCode,
+        results,
+      });
+    });
+  }
+  fetchUser(req, res) {
+    const query = `
         SELECT userID, firstName, lastNmae,userAge, gender,userRole,
         emailAdd, profileUrl
         FROM Users
         WHERE userId = ${req.params.id};
-        `
-        db.query(query,
-            (err, result)=>{
-                if (err) throw err 
-                res.json({
-                    status: res.statusCode,
-                    result
-                })
-            })
-    }
-    login(req, res){    
-    }
-     async register(req, res){
-      const data = req.body 
-      //encrypt password
-      data.userPass = await hash(data.userPass, 15)
-      //PAYLOAD means DATA THAT COMES FROM THE USER
+        `;
+    db.query(query, (err, result) => {
+      if (err) throw err;
+
+      res.json({
+        status: res.statusCode,
+        result,
+      });
+    });
+  }
+  async register(req, res) {
+    try {
+      const data = req.body;
+      data.userPass = await hash(data.userPass, 15);
+
       const user = {
         emailAdd: data.emailAdd,
-        userPass: data.userPass
-      }
-      //query
-      const query = `
-      INSERT INTO Users
-      SET ?; 
-      `
-      db.query(query,[data], (err)=>{
-        if(err) throw err
-        //create a token
-        let token = createToken(user) 
-            res.json({
-                status: res.statusCode,
-                msg: "You are now registered."
-            })
-      })
-    }
-    login(req, res) {
-        const { emailAdd, userPass } = req.body;
-        
-        // Use parameterized query
-        const query = `
-        SELECT userID, firstName, lastNmae,userAge, gender,userRole,
-        emailAdd, profileUrl
-        FROM Users
-        WHERE emailAdd = '${emailAdd}';
-        `;
-        
-        db.query(query, async (err, result) => {
-            if (err) throw err;
-            if (!result?.length) {
-                res.json({
-                    status: res.statusCode,
-                    msg: "You provided a wrong email."
-                });
-            } else {
-                await compare(userPass, result[0].userPass, (cErr, cResult) => {
-                    if (cErr) throw cErr;
-                    // Create a token
-                    const token = createToken({
-                        emailAdd,
-                        userPass
-                    })
-                    if (cResult) {
-                        res.json({
-                            msg: "Logged in",
-                            token,
-                            result: result[0]
-                        });
-                    } else {
-                        res.json({
-                            status: res.statusCode,
-                            msg: "Invalid password or you have not registered"
-                        });
-                    }
-                });
-            }
-        });
-    }
-    
-    updateUser(req, res){
-        const data = req.body
-        if (data.userPass){
-            data.userPass = 
-            hashSync(data.userPass, 15)
+        userPass: data.userPass,
+      };
+
+      const query = "INSERT INTO Users SET ?";
+      db.query(query, user, (err) => {
+        if (err) {
+          res.status(500).json({ msg: "Registration failed." });
+        } else {
+          let token = createToken(user);
+          res.json({ msg: "You are now registered.", token });
         }
-        const query =`
+      });
+    } catch (error) {
+      res.status(500).json({ msg: "Internal server error." });
+    }
+  }
+
+  async login(req, res) {
+    try {
+      const { emailAdd, userPass } = req.body;
+
+      const query = `
+        SELECT userID, firstName, lastName, userAge, gender, userRole,
+        emailAdd, profileUrl, userPass
+        FROM Users
+        WHERE emailAdd = ?;
+      `;
+
+      db.query(query, [emailAdd], async (err, result) => {
+        if (err) {
+          res.status(500).json({ msg: "Internal server error." });
+          return;
+        }
+
+        if (!result.length) {
+          res.json({ msg: "Invalid email." });
+        } else {
+          const isPasswordValid = await compare(userPass, result[0].userPass);
+
+          if (isPasswordValid) {
+            const token = createToken({
+              emailAdd,
+              userPass,
+            });
+            res.json({ msg: "Logged in", token, result: result[0] });
+          } else {
+            res.json({ msg: "Invalid password or not registered." });
+          }
+        }
+      });
+    } catch (error) {
+      res.status(500).json({ msg: "Internal server error." });
+    }
+  }
+  //   async register(req, res) {
+  //     const data = req.body;
+  //     //encrypt password
+  //     data.userPass = await hash(data.userPass, 15);
+  //     //PAYLOAD means DATA THAT COMES FROM THE USER
+  //     const user = {
+  //       emailAdd: data.emailAdd,
+  //       userPass: data.userPass,
+  //     };
+  //     //query
+  //     const query = `
+  //       INSERT INTO Users
+  //       SET ?;
+  //       `;
+  //     db.query(query, [data], (err) => {
+  //       if (err) throw err;
+  //       //create a token
+  //       let token = createToken(user);
+  //       res.json({
+  //         status: res.statusCode,
+  //         msg: "You are now registered.",
+  //       });
+  //     });
+  //   }
+  //   login(req, res) {
+  //     const { emailAdd, userPass } = req.body;
+
+  //     // Use parameterized query
+  //     const query = `
+  //         SELECT userID, firstName, lastNmae,userAge, gender,userRole,
+  //         emailAdd, profileUrl
+  //         FROM Users
+  //         WHERE emailAdd = '${emailAdd}';
+  //         `;
+
+  //     db.query(query, async (err, result) => {
+  //       if (err) throw err;
+  //       if (!result?.length) {
+  //         res.json({
+  //           status: res.statusCode,
+  //           msg: "You provided a wrong email.",
+  //         });
+  //       } else {
+  //         await compare(userPass, result[0].userPass, (cErr, cResult) => {
+  //           if (cErr) throw cErr;
+  //           // Create a token
+  //           const token = createToken({
+  //             emailAdd,
+  //             userPass,
+  //           });
+  //           if (cResult) {
+  //             res.json({
+  //               msg: "Logged in",
+  //               token,
+  //               result: result[0],
+  //             });
+  //           } else {
+  //             res.json({
+  //               status: res.statusCode,
+  //               msg: "Invalid password or you have not registered",
+  //             });
+  //           }
+  //         });
+  //       }
+  //     });
+  //   }
+
+  updateUser(req, res) {
+    const data = req.body;
+    if (data.userPass) {
+      data.userPass = hashSync(data.userPass, 15);
+    }
+    const query = `
         UPDATE User
         SET ?
         WHERE userID = ?
-        `
-        db.query(query,
-            [req.body, req.params.id],
-            (err)=>{
-                if(err) throw err
-                res.json({
-                    status: res.statusCode,
-                    msg: "The user record was updated."
-                })
-            })
-    }
-    deleteUser(req, res){
-        const query = ` DELETE FROM Users 
+        `;
+    db.query(query, [req.body, req.params.id], (err) => {
+      if (err) throw err;
+      res.json({
+        status: res.statusCode,
+        msg: "The user record was updated.",
+      });
+    });
+  }
+  deleteUser(req, res) {
+    const query = ` DELETE FROM Users 
         WHERE userID = ${req.params.id};
-        `
-        db.query(query,(err)=>{
-            if(err) throw err
-            res.json({
-                status: res.statusCode,
-                msg: "A user recored was deleted."
-            })
-        })
-    }
-} 
-module.exports = Users
+        `;
+    db.query(query, (err) => {
+      if (err) throw err;
+      res.json({
+        status: res.statusCode,
+        msg: "A user recored was deleted.",
+      });
+    });
+  }
+}
+module.exports = Users;

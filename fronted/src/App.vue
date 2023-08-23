1a66dd87331195ad093f777024ce2314102024dd
unknown <sisiphopepengweni@gmail.com>
2023-08-23 16:33:25 +0200

    all commits

--- fronted/src/App.vue
+++ fronted/src/App.vue
@@ -18,7 +18,8 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  background-color: #2c3e50;
+  color:white;
 }
 
 nav {
